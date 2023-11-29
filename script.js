9291ae31b7b04b4db449aaf836c82388d5c1b766
anasabdi1 <144709799+anasabdi1@users.noreply.github.com>
2023-11-29 22:17:41 +0000

    DONE

--- script.js
+++ script.js
@@ -1,43 +1,78 @@
-/* Assignment 04: Finishing a Todo List App
- *
- * 
- *
- */
-
-
-//
 // Variables
-//
-
-// Constants
 const appID = "app";
-const headingText = "To do. To done. ✅";
+const headingText = "Your App Heading"; 
 
 // DOM Elements
 let appContainer = document.getElementById(appID);
-
-//
+const todoList = document.getElementById('todo-list');
+const addTodoInput = document.getElementById('add-todo');
+const addTodoButton = document.getElementById('add-todo-button');
+const clearTasksButton = document.getElementById('clear-tasks-button'); 
 // Functions
-//
-
-// Add a heading to the app container
 function inititialise() {
-  // If anything is wrong with the app container then end
   if (!appContainer) {
-    console.error("Error: Could not find app contianer");
+    console.error("Error: Could not find app container");
     return;
   }
 
-  // Create an h1 and add it to our app
   const h1 = document.createElement("h1");
   h1.innerText = headingText;
   appContainer.appendChild(h1);
 
-  // Init complete
   console.log("App successfully initialised");
 }
 
-//
+function addTodo() {
+  const text = addTodoInput.value.trim();
+  if (text !== '') {
+    const todo = createTodoItem(text);
+    todoList.appendChild(todo);
+    addTodoInput.value = '';
+
+    if (todoList.childElementCount === 1) {
+      clearTasksButton.style.display = 'inline-block';
+    }
+  }
+}
+
+function createTodoItem(text) {
+  const todoItem = document.createElement('section');
+  todoItem.classList.add('todo-item');
+
+  const todoCheckbox = document.createElement('input');
+  todoCheckbox.type = 'checkbox';
+  todoCheckbox.classList.add('checkbox');
+
+  const todoText = document.createElement('section');
+  todoText.textContent = text;
+  todoItem.appendChild(todoCheckbox);
+  todoItem.appendChild(todoText);
+
+  const todoActions = document.createElement('section');
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', function() {
+    todoList.removeChild(todoItem);
+
+    if (todoList.childElementCount === 0) {
+      clearTasksButton.style.display = 'none';
+    }
+  });
+
+  todoActions.appendChild(deleteButton);
+  todoItem.appendChild(todoActions);
+
+  return todoItem;
+}
+
+function clearTasks() {
+  todoList.innerHTML = '';
+  clearTasksButton.style.display = 'none';
+}
+
 // Inits & Event Listeners
-//
 inititialise();
+addTodoButton.addEventListener('click', addTodo);
+clearTasksButton.addEventListener('click', clearTasks);
