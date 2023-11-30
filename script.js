dc34dc0eba51818caf09fa0da65a9295f5566846
anasabdi1 <144709799+anasabdi1@users.noreply.github.com>
2023-11-30 14:46:58 +0000

    (todo list was only working in live preview and not in pages)

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-// Variables
-const appID = "app";
-
-
-// DOM Elements
-let appContainer = document.getElementById(appID);
 const todoList = document.getElementById('todo-list');
 const addTodoInput = document.getElementById('add-todo');
 const addTodoButton = document.getElementById('add-todo-button');
-const clearTasksButton = document.getElementById('clear-tasks-button'); 
-// Functions
-function inititialise() {
-  if (!appContainer) {
-    console.error("Error: Could not find app container");
-    return;
-  }
+const clearTasksButton = document.getElementById('clear-tasks-button');
 
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
-
-  console.log("App successfully initialised");
-}
+addTodoButton.addEventListener('click', addTodo);
+clearTasksButton.addEventListener('click', clearTasks);
 
 function addTodo() {
   const text = addTodoInput.value.trim();
@@ -28,6 +12,7 @@ function addTodo() {
     const todo = createTodoItem(text);
     todoList.appendChild(todo);
     addTodoInput.value = '';
+
 
     if (todoList.childElementCount === 1) {
       clearTasksButton.style.display = 'inline-block';
@@ -56,6 +41,7 @@ function createTodoItem(text) {
   deleteButton.addEventListener('click', function() {
     todoList.removeChild(todoItem);
 
+ 
     if (todoList.childElementCount === 0) {
       clearTasksButton.style.display = 'none';
     }
@@ -71,8 +57,3 @@ function clearTasks() {
   todoList.innerHTML = '';
   clearTasksButton.style.display = 'none';
 }
-
-// Inits & Event Listeners
-inititialise();
-addTodoButton.addEventListener('click', addTodo);
-clearTasksButton.addEventListener('click', clearTasks);
